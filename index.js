1f8dade8290d1a8b61967d2dd13985a286d781c9
Andy Chadbourne <andy@sgcmarketing.co.uk>
2019-12-03 21:15:38 +0000

    Changed to class

--- index.js
+++ index.js
@@ -7,18 +7,6 @@
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Exports
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-module.exports.closeFunction = closeFunction
-module.exports.lineBreakToConsole = lineBreakToConsole
-module.exports.messageToConsole = messageToConsole
-module.exports.messageToConsoleAndClose = messageToConsoleAndClose
-module.exports.newlineToConsole = newlineToConsole
-module.exports.openFunction = openFunction
-module.exports.closeFunctionFull = closeFunctionFull
-module.exports.errorToConsoleAndClose = errorToConsoleAndClose
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Global variables
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 global.loggingLevel = -1
@@ -29,155 +17,140 @@ global.functionNames = []
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 var lastFunctionName = ''
 
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Local constants
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-const spacesPerTab = 4
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//  errorToConsoleAndClose(errorMessage)
-//      errorMessage: The error message to be displayed
-//
-// Same as messageToConsole, but performs a closeFunction() at the end and throws an error.
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function errorToConsoleAndClose(error) {
-    //
-
-    var spaces = ''
-    if (loggingLevel > -1) {
-        spaces = ' '.repeat(loggingLevel * spacesPerTab + spacesPerTab)
+class Log {
+    constructor() {
+        this.spacesPerTab = 4
+        this.loggingEnabled = true
     }
-    console.log(_formatMessage(spaces + `THROW :(${error.message}`, functionNames[loggingLevel]))
-    console.log(_formatMessage(spaces + `:(${error.stack}`, functionNames[loggingLevel]))
-    closeFunction()
 
-    throw { code: 1, message: error.message }
-}
+    // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    errorToConsoleAndClose(error) {
+        //
+        if (this.loggingEnabled === false) {
+            return
+        }
 
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//  messageToConsole(message, callingFunction)
-//      message: THe message to be displayed
-//      authorisationSummary: The user that is currently authorised via the provided token
-//      callingFunction: String defining the file/function that made the call
-//
-// Log a message to the console.
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function messageToConsole(message) {
-    //
-    var spaces = ''
-    if (loggingLevel > -1) {
-        spaces = ' '.repeat(loggingLevel * spacesPerTab + spacesPerTab)
-    }
-    console.log(_formatMessage(spaces + message, functionNames[loggingLevel]))
-}
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//  messageToConsoleAndClose(message, callingFunction)
-//      message: THe message to be displayed
-//      authorisationSummary: The user that is currently authorised via the provided token
-//      callingFunction: String defining the file/function that made the call
-//
-// Same as messageToConsole, but performs a closeFunction() at the end,.
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function messageToConsoleAndClose(message) {
-    //
-    var spaces = ''
-    if (loggingLevel > -1) {
-        spaces = ' '.repeat(loggingLevel * spacesPerTab + spacesPerTab)
-    }
-    console.log(_formatMessage(spaces + message, functionNames[loggingLevel]))
-    closeFunction()
-}
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//  closeFunction()
-//
-// Close the function.
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function closeFunctionFull() {
-    //
-    var ctr = loggingLevel
-    while (ctr > -1) {
+        var spaces = ''
+        if (loggingLevel > -1) {
+            spaces = ' '.repeat(loggingLevel * this.spacesPerTab + this.spacesPerTab)
+        }
+        console.log(_formatMessage(spaces + `THROW :(${error.message}`, functionNames[loggingLevel]))
+        console.log(_formatMessage(spaces + `:(${error.stack}`, functionNames[loggingLevel]))
         closeFunction()
-        ctr--
+
+        throw { code: 1, message: error.message }
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    messageToConsole(message) {
+        //
+        if (this.loggingEnabled === false) {
+            return
+        }
+
+        var spaces = ''
+        if (loggingLevel > -1) {
+            spaces = ' '.repeat(loggingLevel * this.spacesPerTab + this.spacesPerTab)
+        }
+        console.log(_formatMessage(spaces + message, functionNames[loggingLevel]))
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    messageToConsoleAndClose(message) {
+        //
+        if (this.loggingEnabled === false) {
+            return
+        }
+
+        var spaces = ''
+        if (loggingLevel > -1) {
+            spaces = ' '.repeat(loggingLevel * this.spacesPerTab + this.spacesPerTab)
+        }
+        console.log(_formatMessage(spaces + message, functionNames[loggingLevel]))
+        this.closeFunction()
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    closeFunctionFull() {
+        //
+        if (this.loggingEnabled === false) {
+            return
+        }
+
+        var ctr = loggingLevel
+        while (ctr > -1) {
+            this.closeFunction()
+            ctr--
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    closeFunction() {
+        //
+        if (this.loggingEnabled === false) {
+            return
+        }
+
+        var spaces = ''
+        if (loggingLevel > 0) {
+            spaces = ' '.repeat(loggingLevel * this.spacesPerTab)
+        }
+
+        console.log(_formatMessage(spaces + '}', functionNames[loggingLevel], false))
+        loggingLevel--
+        functionNames.pop()
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    openFunction(message, callingFunction) {
+        //
+        if (this.loggingEnabled === false) {
+            return
+        }
+
+        functionNames.push(callingFunction)
+        loggingLevel++
+
+        lastFunctionName = ''
+
+        var spaces = ''
+        if (loggingLevel > 0) {
+            spaces = ' '.repeat(loggingLevel * this.spacesPerTab)
+        }
+        console.log(_formatMessage(spaces + message + ' {', functionNames[loggingLevel], true))
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    lineBreakToConsole() {
+        //
+        if (this.loggingEnabled === false) {
+            return
+        }
+
+        const numberOfCharactersInLine = 86
+        // Character codes set colour to yellow and then reset
+        console.log('\x1b[33m' + '-'.repeat(numberOfCharactersInLine) + '-\x1b[0m')
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    newlineToConsole(numberOfLines) {
+        //
+        if (this.loggingEnabled === false) {
+            return
+        }
+
+        if (typeof numberOfLines === 'undefined' || numberOfLines <= 0) {
+            numberOfLines = 1
+        }
+
+        for (var ctr = 0; ctr < numberOfLines; ctr++) {
+            console.log('')
+        }
     }
 }
 
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//  closeFunction()
-//
-// Close the function.
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function closeFunction() {
-    //
-    var spaces = ''
-    if (loggingLevel > 0) {
-        spaces = ' '.repeat(loggingLevel * spacesPerTab)
-    }
+module.exports = Log
 
-    console.log(_formatMessage(spaces + '}', functionNames[loggingLevel], false))
-    loggingLevel--
-    functionNames.pop()
-}
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//  functionToConsole(message, callingFunction)
-//      message: THe message to be displayed
-//      authorisationSummary: The user that is currently authorised via the provided token
-//      callingFunction: String defining the file/function that made the call
-//
-// Log a message to the console and indent the rest of the messages until a closeFunction() is performed.
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function openFunction(message, callingFunction) {
-    //
-    functionNames.push(callingFunction)
-    loggingLevel++
-
-    lastFunctionName = ''
-
-    var spaces = ''
-    if (loggingLevel > 0) {
-        spaces = ' '.repeat(loggingLevel * spacesPerTab)
-    }
-    console.log(_formatMessage(spaces + message + ' {', functionNames[loggingLevel], true))
-}
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//  lineBreakToConsole()
-//
-// Write a line to the console
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function lineBreakToConsole() {
-    //
-    const numberOfCharactersInLine = 86
-    // Character codes set colour to yellow and then reset
-    console.log('\x1b[33m' + '-'.repeat(numberOfCharactersInLine) + '-\x1b[0m')
-}
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// newlineToConsole (numberOfLines)
-//      numberOfLines: Number of lines to output
-//      callback: The function to call on completion
-//
-// Write a blank line to the console.
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function newlineToConsole(numberOfLines) {
-    //
-    if (typeof numberOfLines === 'undefined' || numberOfLines <= 0) {
-        numberOfLines = 1
-    }
-
-    for (var ctr = 0; ctr < numberOfLines; ctr++) {
-        console.log('')
-    }
-}
-
-// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// _formatMessage(message, callingFile)
-//      message: The message to be displayed
-//      callingFile: String defining the file/function that made the call
-//
-// Format the message prior to display.
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function _formatMessage(message, callingFile, isFunction) {
     //
